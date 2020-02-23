027aab070ea9bfbf8cbba2165d7ed49501b91552
Sergey Borisevich <borisevich.serjyk@gmail.com>
2020-02-23 22:00:30 +0300

    feat: task is resolved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (arguments.length == 0 || array.length == 0) return 0;
+  let min = 0;
+  for (let i = 0; i <= array.length; i++) {
+    if (min > array[i]) min = array[i];
+  }
+  return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (arguments.length == 0 || array.length == 0) return 0;
+  let max = 0;
+  for (let i = 0; i <= array.length; i++) {
+    if (max < array[i]) max = array[i];
+  }
+  return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (arguments.length == 0 || array.length == 0) return 0;
+  return array.reduce((sum, cur) => sum += cur, 0) / array.length;
 }
